73ba5c41a29b38b35a790df1728fa5c5f2c03a8b
kharvirakesh <kharvirakesh3.com>
2023-12-07 02:30:37 +0530

    FEAT : Implemented skeletal structure for the product page

--- ecommerce-webapp/src/pages/product/index.tsx
+++ ecommerce-webapp/src/pages/product/index.tsx
@@ -1,15 +1,31 @@
-
 import React from "react";
 import Head from "next/head";
 import { BreadCrumb } from "@/component/Common/BreadCrumb/BreadCrumb";
 import styles from '../../styles/product.module.css';
+import { Col, Row } from "antd";
 
-export default function Product() {
+const Product = () => {
   const pageName = 'product';
+  const pageTitle = `${pageName.charAt(0).toUpperCase()}${pageName.slice(1) ?? ''} Page`;
+
+  const renderProductItems = (count: number) => {
+    const items = [];
+    for (let i = 0; i < count; i++) {
+      items.push(
+        <Col key={i} span={6} style={{ height: '225px' }}>
+          <div style={{ margin: '20px', height: '80%', background: '#d9d9d9', borderRadius: '10px', textAlign: 'center', }}>
+            {/* Product Item {i + 1} */}
+          </div>
+        </Col>
+      );
+    }
+    return items;
+  };
+
   return (
     <div>
       <Head>
-        <title>{pageName.charAt(0).toUpperCase() + pageName.slice(1) ?? ''} Page</title>
+        <title>{pageTitle} </title>
         <meta name="description" content="New description for your product page" />
         <meta name="keywords" content="product, features, description, keywords" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -17,9 +33,20 @@ export default function Product() {
       </Head>
       <div className={styles.container}>
         <BreadCrumb currentPage={pageName ?? ''} />
+
+        <Row style={{ marginTop: '20px' }}>
+          <Col span={18} push={6} style={{ borderRadius: '8px', height: 'auto', background: '#598892' }}>
+            <Row gutter={[20, 20]}>
+              {renderProductItems(12)}
+            </Row>
+          </Col>
+          <Col span={5} pull={18} style={{ borderRadius: '8px', height: 'auto', background: '#598892' }}>
+            {/* Filters */}
+          </Col>
+        </Row>
       </div>
     </div>
   );
 };
 
-
+export default Product;
